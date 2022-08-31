7fd8ebbdcdec15b9668e7e54edfc58603a2e00df
brl66 <brl66@txstate.edu>
2022-08-31 12:40:10 -0500

    Preparing the ShoeTest to contain a BlackJackShoe. Not done.

--- app/debug/ShoeTest.java
+++ app/debug/ShoeTest.java
@@ -2,11 +2,13 @@ package app.debug;
 
 import app.menu.*;
 import animation.*;
+import arcade.game.blackjack.blackjack_items.*;
 import javax.swing.*;
 import java.awt.*;
 
 public class ShoeTest extends JPanel implements IMenu{
 	private Animation bg;
+	private BlackJackShoe sh;
 	public ShoeTest(int width_px, int height_px){
 		setSize(width_px,height_px);
 		setLayout(null);
--- arcade/game/blackjack/blackjack_items/BlackJackShoe.java
+++ arcade/game/blackjack/blackjack_items/BlackJackShoe.java
@@ -1,9 +1,22 @@
 package arcade.game.blackjack.blackjack_items;
 
+import java.util.ArrayList;
 import arcade.game_items.Card;
 import arcade.game_items.Shoe;
 
 public class BlackJackShoe implements Shoe{
+	private ArrayList<BlacJackCard> list;
+	
+	public BlackJackShoe(){ //single deck implementation
+		Shoe tmp=genStandardDeck();
+
+		BlackJackCard upgrade;
+		while(tmp.getSize()>0){
+			upgrade = new BlackJackCard(tmp.remove(0));
+			list.add(upgrade);
+		}
+	}
+
     @Override
     public Card drawTop(){return null;}
 }
