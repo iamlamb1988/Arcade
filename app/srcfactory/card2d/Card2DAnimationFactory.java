e109c511df2129ab3d5f841a01120a335688771a
brl66 <brl66@txstate.edu>
2022-09-04 20:01:03 -0500

    Refactor Card Animation classes a little more.

--- app/srcfactory/card2d/Card2DAnimationFactory.java
+++ app/srcfactory/card2d/Card2DAnimationFactory.java
@@ -1,21 +1,56 @@
 package app.srcfactory.card2d;
 
 import app.srcfactory.AnimationFactory;
+import animation.SingleImage;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 //This class preserves Card Images
 public class Card2DAnimationFactory extends AnimationFactory{
+	//Ratios for Standard Card (3.5" x 2.5")
+	private final byte LEN_RATIO;
+	private final byte WDT_RATIO;
+
 	private BufferedImage bg;
 	private BufferedImage BGborder;
 	private BufferedImage BackBG;
+
+	
 	private boolean isUpSeparate; //Are the bg and BGborder separate?
 
 	public Card2DAnimationFactory(){
 		super();
+		LEN_RATIO=(byte)7;
+		WDT_RATIO=(byte)5;
 		isUpSeparate=false;
 	}
 
-	public void mergeBGandBorder(){}
+	//if overlapTL, will overlap images at the top left (TL) corner.
+	public void mergeBGandBorder(boolean overlapTL){
+		
+	}
 
-	
+	//Should this function move to Card2DAnimationFactory?
+	public SingleImage genCardImage2D(char face, char suit,
+		int width_px, int height_px,
+		int borderThickness_px){
+		BufferedImage R = new BufferedImage(width_px,height_px,BufferedImage.TYPE_INT_ARGB);
+		Graphics2D P = (Graphics2D)R.createGraphics();
+
+		//Draw white backrground
+		P.setColor(new Color(255,255,255,255));
+		P.fillRect(0,0,width_px,height_px);
+
+		//Draw Blue Border
+		P.setColor(new Color(0,255,255,255)); //cyan
+		P.fillRect(0,0,borderThickness_px,height_px); //Left
+		P.fillRect(width_px-borderThickness_px,0,borderThickness_px,height_px); //Right
+		P.fillRect(0,0,width_px,borderThickness_px); //Top
+		P.fillRect(0,height_px-borderThickness_px,width_px,borderThickness_px); //Bottom
+
+		//next?
+		return new SingleImage(R);
+	}
 }
