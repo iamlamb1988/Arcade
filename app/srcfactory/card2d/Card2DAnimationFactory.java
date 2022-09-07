d1c74eeed183bc72167bb914d8b8f155fa027549
brl66 <brl66@txstate.edu>
2022-09-07 12:26:16 -0500

    DOES NOT COMPILE. In progress of developing animation.

--- app/srcfactory/card2d/Card2DAnimationFactory.java
+++ app/srcfactory/card2d/Card2DAnimationFactory.java
@@ -4,6 +4,7 @@ import app.srcfactory.AnimationFactory;
 import animation.Animation;
 import animation.SingleImage;
 import resources.CardImgFetcher;
+import java.lang.Math;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.awt.Graphics2D;
@@ -28,7 +29,7 @@ public class Card2DAnimationFactory extends AnimationFactory{
 	private CardImgFetcher cif;
 
 	private Animation bg;
-	private Animation BackBG;
+	private Animation BackBG; //Not yet used...
 
 	//Redundant? No because of Animation.
 	private Animation[] faceImg;
@@ -67,6 +68,27 @@ public class Card2DAnimationFactory extends AnimationFactory{
 		cif.fetchAll(width_px,height_px);
 		actualWidth_px=width_px;
 		actualHeight_px=height_px;
+	}
+
+	private byte getFaceIndex(char face){
+		if(face=='T') return (byte)8;
+		else if(face=='J') return (byte)9;
+		else if(face=='Q') return (byte)10;
+		else if(face=='K') return (byte)11;
+		else if(face=='A') return (byte)12;
+
+		byte value=(byte)(face-'2');
+		if(value < 0 || value > 7) return -1;
+		else return value;
+	}
+
+	private byte getSuitIndex(char suit){
+		if(suit=='C') return 0;
+		else if(suit=='D') return 1;
+		else if(suit=='H') return 2;
+		else if(suit=='S') return 3;
+
+		return -1;
 	}
 
 	public boolean isBG_Set(){return bg!=null;}
@@ -123,13 +145,34 @@ public class Card2DAnimationFactory extends AnimationFactory{
 	}
 
 	//Full gen requires all sub images and dimensions set
-	public SingleImage genCard2D(char face, char suit){
+	public void setCard2D(char face, char suit){
+		byte Index;
 		BufferedImage R = new BufferedImage(actualWidth_px,actualHeight_px,BufferedImage.TYPE_INT_ARGB);
-		BufferedImage TR = new BufferedImage(actualWidth_px,actualHeight_px/2,BufferedImage.TYPE_INT_ARGB);
-		Graphics2D P=TR.createGraphics();
+		BufferedImage TR = new BufferedImage(actualWidth_px,actualHeight_px/2,BufferedImage.TYPE_INT_ARGB),
+					  BR = new BufferedImage(actualWidth_px,actualHeight_px/2,BufferedImage.TYPE_INT_ARGB);
 
-		
-		return null;
+		//Draw Top Half TR -> Top Return
+		Graphics2D P=TR.createGraphics();
+		bg.drawTopLeft(P);
+
+		//Draw face
+		faceImg[getFaceIndex(face)].draw(actualSideOffset_px,actualTopOffset_px,P);
+
+		//draw suit
+		suitImg[getSuitIndex(suit)].draw(actualWidth_px=actualSideOffset_px,actualTopOffset_px);
+
+		//Prepare rotation of 180 and shift half of height
+		AffineTransform T = new AffineTransform();
+		T.rotate(Math.PI,actualWidth_px/2,actualHeight_px/2);
+		P=BR.getGraphics();
+		while(!P.drawImage(TR,T,null)){}
+
+		//draw to finalized set
+		P=R.createGraphics();
+		while(!P.drawImage(TR,0,0,null)){}
+		while(!P.drawImage(TR,0,TR.getHeight(),null){}
+
+		//set to standard index value
 	}
 
 	public SingleImage genCardFace(char face){return new SingleImage(cif.getCardFace(face));}
