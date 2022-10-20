eab030719e221fb555c6b90b88b24222f1e88c1e
brl66 <brl66@txstate.edu>
2022-10-20 12:01:56 -0500

    Preparing to refactor BlackJackTable constructor for more robust Card Types.

--- app/release/sandbox/tablecycle/TableCycle.java
+++ app/release/sandbox/tablecycle/TableCycle.java
@@ -3,10 +3,14 @@ package app.release.sandbox.tablecycle;
 import app.menu.gamemenu.GameMenu;
 import app.DesktopApp;
 import app.menu.IMenu;
+import arcade.game.blackjack.blackjack_items.BlackJackTable;
+import arcade.game.blackjack.blackjack_items.instances.BlackJackTable_Default;
 
 public class TableCycle extends GameMenu{
+	private BlackJackTable t;
+	
 	public TableCycle(DesktopApp parentApp, IMenu previous, int width_px, int height_px){
 		super(parentApp,previous,width_px,height_px);
-		
+		t=new BlackJackTable_Default();
 	}
 }
