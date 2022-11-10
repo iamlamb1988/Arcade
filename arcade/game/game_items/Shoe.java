a206618597f6dbdc123ca4f2e6b703677bf8545e
brl66 <brl66@txstate.edu>
2022-11-10 11:27:03 -0600

    Fixing duplicate code within BlackJackShoe. Still need to alter return types using wildcard generics. Preparing to upgrade BlackJackShoe with generics.

--- arcade/game/game_items/Shoe.java
+++ arcade/game/game_items/Shoe.java
@@ -4,17 +4,6 @@ import java.util.Collections;
 import java.util.ArrayList;
 
 public interface Shoe{
-	public static ArrayList<Card> genStandardDeck(){
-		ArrayList<Card> R = new ArrayList<Card>(52);
-
-		char[] suits = new char[]{'C','D','H','S'};
-		char[] faces = new char[]{'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
-
-		for(char x : suits){for(char y : faces){R.add(new Card(y,x));}}
-
-		return R;
-	}
-
 	public static ArrayList<Card> genStandardDecks(byte num_of_decks){
 		ArrayList<Card> R = new ArrayList<Card>(52*num_of_decks);
 		num_of_decks--;
@@ -35,6 +24,8 @@ public interface Shoe{
 
 		return R;
 	}
+
+	public static ArrayList<Card> genStandardDeck(){return genStandardDecks((byte)1);}
 
 	public static void shuffleShoe(ArrayList<Card> unshuffledShoe){
 		Collections.shuffle(unshuffledShoe);
